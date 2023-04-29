b6d6957c6256f78e180d1d356bd2fcef828aceb6
Furkan Cengiz <muhammedcengiz1@gmail.com>
2023-04-29 13:14:12 +0300

    add: input validation for nums
    letters are forbidden not to cause issues.
    elapsed days and days are length of 30 and 31 are checked.
    validation messages will be added but Must be a valid date message was added to test validations.

--- age-calculator-app-main/index.js
+++ age-calculator-app-main/index.js
@@ -5,19 +5,31 @@ const yearInputEl = document.querySelector('#year');
 const resultYear = document.querySelector('#elapsedYears');
 const resultMonth = document.querySelector('#elapsedMonths');
 const resultDay = document.querySelector('#elapsedDays');
+const dayValidationEl = document.querySelector('.dayValidation');
+const monthValidationEl = document.querySelector('.monthValidation');
+const yearValidationEl = document.querySelector('.yearValidation');
+setValidation(dayInputEl, 1, 31);
+setValidation(monthInputEl, 1, 12);
+setValidation(yearInputEl, 0, 2022);
 formEl.addEventListener('submit', (e) => {
     e.preventDefault();
     const day = dayInputEl.value;
     const month = monthInputEl.value;
     const year = yearInputEl.value;
     const currentDate = new Date();
-    // debugger
     const currentYear = currentDate.getFullYear();
-    const currentMonth = currentDate.getMonth();
+    const currentMonth = currentDate.getMonth() + 1;
     const currentDay = currentDate.getDate();
     let elapsedYears = currentYear - year;
     let elapsedMonths = currentMonth - month;
     let elapsedDays = currentDay - day;
+    let message = validateDate(day, month, year) ? undefined : 'Must be a valid date';
+    if(message){
+        setValidationError(dayValidationEl, message);
+        return
+    }else{
+        setValidationError(dayValidationEl, '');
+    }
     if(elapsedDays < 0) {
         elapsedMonths--;
         elapsedDays += 30;
@@ -29,5 +41,43 @@ formEl.addEventListener('submit', (e) => {
     resultYear.textContent = elapsedYears;
     resultMonth.textContent = elapsedMonths;
     resultDay.textContent = elapsedDays;
-
 })
+function setValidation(inputEl, min, max){
+    inputEl.addEventListener('input', e => {
+        e.target.value = e.target.value.replace(/[^0-9][^s]/g, '');
+        if(e.target.value < min) {
+            e.target.value = ''
+        }
+        if(e.target.value > max) {
+            e.target.value = max
+        }
+    })
+}
+function validateDate(day, month, year) {
+    month = parseInt(month)
+    day = parseInt(day)
+    let listOfDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    if(month == 1 || month > 2){
+        if(day > listOfDays[month - 1]){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+    else{
+        let isYearElapsed = false;
+        if(year % 4 == 0 && year % 100 != 0 || year % 400 == 0){
+            isYearElapsed = true;
+        }
+        if(isYearElapsed && day > 29){
+            return false;
+        }
+        if(!isYearElapsed && day > 28){
+            return false;
+        }
+    }
+}
+function setValidationError(spanEl, message) {
+    spanEl.textContent = message;
+}
