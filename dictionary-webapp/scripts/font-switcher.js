0f1c5df4fd738f1bb37cea4197e6ec1647eeafc3
Furkan Cengiz <muhammedcengiz1@gmail.com>
2023-05-19 22:30:51 +0300

    fix: dropdown menu width and text alignment

--- dictionary-webapp/scripts/font-switcher.js
+++ dictionary-webapp/scripts/font-switcher.js
@@ -1,5 +1,6 @@
 export const navDropdown = document.querySelector(".nav__dropdown");
 export const navDropdownList = document.querySelector(".nav__dropdown__list");
+const selectedFontSpan = document.querySelector("#selected__font");
 const navDropdownListItems = document.querySelectorAll(
   ".nav__dropdown__list__item"
 );
@@ -8,10 +9,13 @@ for (const listItem of navDropdownListItems) {
   listItem.addEventListener("click", () => {
     if (listItem.innerText === "Mono") {
       setSelectedFontFamily("Inconsolata");
+      selectedFontSpan.innerText = "Mono";
     } else if (listItem.innerText === "Sans Serif") {
       setSelectedFontFamily("Inter");
+      selectedFontSpan.innerText = "Sans Serif";
     } else {
       setSelectedFontFamily("Lora");
+      selectedFontSpan.innerText = "Serif";
     }
   });
 }
