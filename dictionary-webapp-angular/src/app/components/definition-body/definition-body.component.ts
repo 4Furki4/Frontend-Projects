12a6d4f50095ea573f81ade0387a054220a46d18
Furkan Cengiz <muhammedcengiz1@gmail.com>
2023-05-25 12:32:18 +0300

    add: routing and ipnut that are fully functional! in addition that when form submitted using enter key, focus is being removed not to cause input validation error.

--- dictionary-webapp-angular/src/app/components/definition-body/definition-body.component.ts
+++ dictionary-webapp-angular/src/app/components/definition-body/definition-body.component.ts
@@ -1,9 +1,8 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { WordRequestService } from '../../services/word-request.service';
 import { WordResponse } from '../../interfaces/word-response';
-import { Word } from '../../interfaces/word';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-definition-body',
@@ -11,31 +10,18 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./definition-body.component.scss']
 })
 export class DefinitionBodyComponent implements OnInit {
-  constructor(private wordRequest: WordRequestService, private activatedRoute: ActivatedRoute) {
-    console.log("DefinitionBodyComponent");
-  }
-  wordFromParam !: string;
-  async ngOnInit(): Promise<void> {
-    this.activatedRoute.paramMap.subscribe((param) => {
-      this.wordFromParam = param.get("word") ?? ""
-    })
-    if (this.wordFromParam.length > 0) {
-      this.wordResponse = await this.wordRequest.getWordMeaning(this.wordFromParam)
-    }
-  }
-  wordInput: Word = new Word("")
-  @ViewChild("searchForm") searchForm!: NgForm;
-  @ViewChild("definitions") definition!: ElementRef<HTMLDivElement>;
-  @ViewChild("definitionHead") definitionHead!: ElementRef<HTMLDivElement>;
-  @ViewChild("definitionBody") definitionBody!: ElementRef<HTMLDivElement>;
-
+  constructor(private wordRequest: WordRequestService, private activatedRoute: ActivatedRoute) { }
   wordResponse !: Array<WordResponse>;
   wordTitle !: string;
-
-
-  public get isInputValid(): boolean {
-    let wordFiltered = this.wordInput.word?.trim()
-    return wordFiltered?.length > 0 ? true : false;
+  wordFromParam !: string;
+  async ngOnInit(): Promise<void> {
+    this.activatedRoute.paramMap.subscribe(async (param) => {
+      this.wordFromParam = param.get("word") ?? ""
+      if (this.wordFromParam.length > 0) {
+        this.wordResponse = await this.wordRequest.getWordMeaning(this.wordFromParam)
+        this.wordTitle = this.wordResponse[0].word
+      }
+    })
   }
   playSound(soundLink: string) {
     let audio = new Audio(soundLink);
--- dictionary-webapp-angular/src/app/components/search-bar/search-bar.component.ts
+++ dictionary-webapp-angular/src/app/components/search-bar/search-bar.component.ts
@@ -1,5 +1,6 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, HostListener, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Route, Router } from '@angular/router';
 import { Word } from 'src/app/interfaces/word';
 import { WordResponse } from 'src/app/interfaces/word-response';
 import { WordRequestService } from 'src/app/services/word-request.service';
@@ -10,16 +11,22 @@ import { WordRequestService } from 'src/app/services/word-request.service';
   styleUrls: ['./search-bar.component.scss']
 })
 export class SearchBarComponent {
-  constructor(private wordRequest: WordRequestService) {
-    console.log("SearchBarComponent");
-  }
+  constructor(private wordRequest: WordRequestService, private route: Router) { }
   @ViewChild("searchForm") searchForm!: NgForm;
   wordInput: Word = new Word("")
   wordResponse !: Array<WordResponse>;
   async onSubmit() {
     const word = this.wordInput.word;
-    this.wordResponse = await this.wordRequest.getWordMeaning(word)
-    this.searchForm.reset()
+    this.route.navigate([`${word}`])
+  }
+  @HostListener('window:keydown.enter', ['$event'])
+  onEnter(event: KeyboardEvent) {
+    if (event.target instanceof HTMLInputElement) {
+      // when word searched by pressing enter on input element, submit the form and change the focus so that input validation error doesn't appear when not focused
+      this.onSubmit()
+      event.target.blur()
+      this.searchForm.reset()
+    }
   }
   public get isInputValid(): boolean {
     let wordFiltered = this.wordInput.word?.trim()
